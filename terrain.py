862b2fb0fb30f32d87ff5d20a960af28f75a407f
toshit <toshit307@gmail.com>
2020-06-07 17:29:06 -0700

    Revert "add globals and blocks in seperate file"
    This reverts commit f82719876f235852ae6731d51931ce057b9430c9.

--- terrain.py
+++ terrain.py
@@ -1,19 +1,9 @@
 from noise import snoise2
-import math
-import globals as G
 
-
-def normal_round(n):
-    if n - math.floor(n) < 0.5:
-        return math.floor(n)
-    return math.ceil(n)
-
-
-def perlin(x, y):
-    return normal_round(snoise2(x, y, G.OCTAVES, G.PERSISTENCE, G.LACUNARITY))
-
-# debug
-# if __name__ == "__main__":
-#     for x in range(10):
-#         for y in range(10):
-#             print(abs(perlin(x, y)) * 100)
+def perlin(x,y):
+    #                    octaves  persistence
+    return snoise2(x, y, 10    , 1.25        , 10)
+if __name__ == "__main__":
+    for x in range(10):
+        for y in range(10):
+            print(abs(perlin(x,y))*100)
